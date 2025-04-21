feac4608a194630a269a7eefb5514ef57c839a3e
jonathandfmx <55897938+jonathandfmx@users.noreply.github.com>
2025-04-21 09:25:31 -0600

    Primer commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,13 +1,35 @@
 import "./globals.css";
 
-export default function RootLayout({
+import type { Metadata } from "next";
+import { ClerkProvider } from "@clerk/nextjs";
+import QueryProvider from "@/providers/QueryProvider";
+
+export const metadata: Metadata = {
+  title: "X Clone",
+  description: "Next.js social media application project",
+};
+
+export default function AppLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body>{children}</body>
-    </html>
+    <ClerkProvider>
+      <QueryProvider>
+        <html lang="en">
+          <body>{children}</body>
+        </html>
+      </QueryProvider>
+    </ClerkProvider>
+    // <html lang="en">
+    //   <body>
+    //     <div className="flex justify-between bg-red-300 sm:bg-blue-300 md:bg-green-300">
+    //       <div className="">Left</div>
+    //       <div className="">{children}</div>
+    //       <div className="">Right</div>
+    //     </div>
+    //   </body>
+    // </html>
   );
 }
