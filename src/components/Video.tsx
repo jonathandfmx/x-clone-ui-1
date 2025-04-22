d07a0aea8adcd46e2dd099292ef75775ee15fa68
jonathandfmx <55897938+jonathandfmx@users.noreply.github.com>
2025-04-22 12:10:33 -0600

    Changes before building

--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -15,7 +15,7 @@ const Video = ({ path, className }: VideoTypes) => {
       path={path}
       className={className}
       transformation={[
-        { width: "1920", height: "1080", q: "90" },
+        { width: "1920", height: "1080" },
         { raw: "l-text,i-JonaDev,fs-100,co-white,l-end" },
       ]}
       controls
